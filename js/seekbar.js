7cadda9a7728f412b7d34de3f497141e8f8c4f0d
marc <19379@users.noreply.github.com>
2015-10-08 23:16:09 +0100

    seekbar.js: adjust repaint area

--- js/seekbar.js
+++ js/seekbar.js
@@ -1,7 +1,7 @@
 _.mixin({
 	seekbar : function (x, y, w, h) {
 		this.playback_seek = function () {
-			window.RepaintRect(0, this.y, window.Width, this.h);
+			window.RepaintRect(this.x - 100, this.y, this.w + 100, this.h);
 		}
 		
 		this.playback_stop = function () {
