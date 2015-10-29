0ca2631dbd746220f1138cbcd385ad890d91db82
marc <19379@users.noreply.github.com>
2015-10-29 15:49:30 +0000

    support custom tooltip font through properties dialog

--- js/helpers.js
+++ js/helpers.js
@@ -42,7 +42,11 @@ var fso = new ActiveXObject("Scripting.FileSystemObject");
 var vb = new ActiveXObject("ScriptControl");
 vb.Language = "VBScript";
 
-var tooltip = window.CreateTooltip();
+//check if v1.6.1 or later
+if ("GetQueryItems" in plman)
+	var tooltip = window.CreateTooltip(window.GetProperty("2K3.TOOLTIP.FONT.NAME", "Segoe UI"), window.GetProperty("2K3.TOOLTIP.FONT.SIZE", 12), window.GetProperty("2K3.TOOLTIP.FONT.STYLE", 0));
+else 
+	var tooltip = window.CreateTooltip();
 tooltip.SetMaxWidth(800);
 
 var drive = fb.ProfilePath.substring(0, 3);
